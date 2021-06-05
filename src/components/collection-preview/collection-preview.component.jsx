bf31ac362c8b0e1dd1fffbc78ee47a9ad4b0cab3
anupmaharzn <hellen7203@gmail.com>
2021-06-05 17:54:06 +0545

    updated shop data with new data ,updated cart utlis to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,8 +8,8 @@ const CollectionPreview = ({title , items}) => (
      {
          items
              .filter((item , idx)=> idx <4)
-                 .map ( ({id, ...otheritemprops}) => (
-                    <CollectionItem key={id} {...otheritemprops}/>
+                 .map ( (item) => (
+                    <CollectionItem key={item.id} item={item}/>
                  ))
      }
  </div>
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
-
+import {addItemToCart} from "./cart.utils";
 import {CartActionTypes} from "./cart.types";
 const INITIAL_STATE = {
-    hidden:true
+    hidden:true,
+    cartItems:[]
 };
 
 const cartReducer = (state = INITIAL_STATE,action)=>{
@@ -12,6 +13,11 @@ const cartReducer = (state = INITIAL_STATE,action)=>{
             return {
                 ...state,
                 hidden: !state.hidden
+            };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems,action.payload) //action.payload is item we want to add
             }
         default:
             return state;
